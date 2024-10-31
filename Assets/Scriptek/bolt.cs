362e1f5ced9ee1db55a1e6dd9f8103bbdbd68184
katbengod <katona.bence@bottyan.eu>
2024-10-31 19:55:52 +0100

    Bolt
    Kész a bolt is, 'E'-vel lehet belépni és 'ESC'-el ki. Még a dizájnon van mint finomítani.

--- Assets/Scriptek/bolt.cs
+++ Assets/Scriptek/bolt.cs
@@ -1,38 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bolt : MonoBehaviour
 {
-    public static bool BoltActive = false;
-    public GameObject ShopsUI;
+    public static bool BoltActive = false; // Annak nyilvántartása, hogy a bolt aktív-e
+    [SerializeField] private GameObject shopUI; // A vásárlói felület Canvas eleme
 
-    void OnTriggerEnter(Collider other)
+    private bool isPlayerInRange = false; // Jelzi, ha a játékos a bolt közelében van
+
+    private void Start()
     {
-        if (other.CompareTag("Player"))
+        shopUI.SetActive(false); // Kezdetben láthatatlan a boltfelület
+    }
+
+    private void Update()
+    {
+        // Ha a játékos a bolt közelében van, és megnyomja az "E" gombot
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            if (BoltActive)
-            {
-                Vissza();
-            }
-            else 
-            {
-                Pause();
-            }
+            ToggleShop();
+        }
+
+        // Ha a bolt aktív, és megnyomja az "ESC" gombot, bezárjuk a boltot
+        if (BoltActive && Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseShop();
         }
     }
 
-    void Vissza() 
+    private void ToggleShop()
     {
-        ShopsUI.SetActive(false);
-        Time.timeScale = 1f;
+        // A boltfelület láthatóságának kapcsolása
+        if (BoltActive)
+        {
+            CloseShop();
+        }
+        else
+        {
+            OpenShop();
+        }
+    }
+
+    private void OpenShop()
+    {
+        shopUI.SetActive(true); // Boltfelület megjelenítése
+        Time.timeScale = 0f; // Játék megállítása
+        BoltActive = true;
+    }
+
+    private void CloseShop()
+    {
+        shopUI.SetActive(false); // Boltfelület elrejtése
+        Time.timeScale = 1f; // Játék folytatása
         BoltActive = false;
     }
 
-    void Pause() 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        ShopsUI.SetActive(true);
-        Time.timeScale = 0f;
-        BoltActive = true;
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = true; // A játékos a bolt trigger területén belül van
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false; // A játékos elhagyta a bolt trigger területét
+            if (BoltActive) CloseShop(); // Bezárjuk a boltot, ha a játékos kilép a területbõl
+        }
     }
 }
